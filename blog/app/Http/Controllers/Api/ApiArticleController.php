27f5ef30da86b121a2bc33fea4905ff5773a764a
LahiruSandaruwan <lsblackcyber@gmail.com>
2017-05-05 10:15:25 +0530

    create basic structure for controllers

---
+++ blog/app/Http/Controllers/Api/ApiArticleController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Routing\Controller as BaseController;
+
+
+class ApiArticleController extends BaseController
+{
+
+    public function getArticle($id=0){
+
+    }
+    
+    public function createArticle(){
+        
+    }
+    
+    public function editArticle($id=0){
+        
+    }
+    
+    public function deleteArticle($id=0){
+        
+    }
+}
---
+++ blog/app/Http/Controllers/Api/ApiAuthorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Routing\Controller as BaseController;
+
+
+class ApiAuthorController extends BaseController
+{
+
+    public function createAuthor(){
+        
+    }
+    
+}
